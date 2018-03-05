caec2c40029722b44383484503fdbb3cec986967
Wagner Morais Domingues <wagner.morais@gmail.com>
2018-03-05 11:29:47 -0300

    Generated with JHipster 4.14.0

---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/pt';
+
+import {
+    BlogwagnerSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        BlogwagnerSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        JhiLanguageHelper,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'pt'
+        },
+    ],
+    exports: [
+        BlogwagnerSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class BlogwagnerSharedCommonModule {
+    constructor() {
+        registerLocaleData(locale);
+    }
+}
