beb3bfdfa92240c9f5ee0a0f7c922b57eea8b340
Wagner Morais Domingues <wagner.morais@gmail.com>
2018-05-25 16:10:11 +0100

    Generated with JHipster 4.14.1

---
+++ src/main/java/com/binside/web/rest/errors/LoginAlreadyUsedException.java
@@ -0,0 +1,8 @@
+package com.binside.web.rest.errors;
+
+public class LoginAlreadyUsedException extends BadRequestAlertException {
+
+    public LoginAlreadyUsedException() {
+        super(ErrorConstants.LOGIN_ALREADY_USED_TYPE, "Login already in use", "userManagement", "userexists");
+    }
+}
